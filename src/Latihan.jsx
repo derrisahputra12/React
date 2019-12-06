7c090241220acc8c16f2edda7d31c155d2607c69
derrisahputra12 <derrisahputra@gmail.com>
2019-12-06 09:44:54 +0700

    Update File

---
+++ src/Latihan.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+export class Latihan extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto excepturi dolor maxime deleniti aperiam quam, dolores dignissimos, in error distinctio ducimus. Veritatis corrupti placeat natus rem iure perferendis at facere!
+                Quae veritatis vel autem molestias magnam id deleniti quibusdam ratione neque atque deserunt asperiores, cum reiciendis similique sit! Suscipit consectetur vero iusto esse, asperiores quo. Corrupti obcaecati ut quisquam saepe?
+                Eius, nostrum exercitationem, repellendus excepturi ab quaerat accusantium consequatur, asperiores magni ipsum quae incidunt commodi deserunt minus accusamus iure eaque consectetur reiciendis dolor. Placeat itaque dicta quisquam veniam explicabo! Numquam!
+                Temporibus recusandae eius ad omnis fugiat, culpa ab distinctio, ullam a dolor repellat ut labore suscipit quo praesentium nisi dignissimos animi ducimus laudantium. Placeat error vero eius non reiciendis iure?
+                Eveniet adipisci non numquam laboriosam obcaecati odit, provident sapiente autem assumenda corrupti, harum nobis delectus maxime iure illo dolorum itaque suscipit quibusdam aliquam magnam. At delectus praesentium illo. Rerum, sapiente?
+                Voluptate officia nulla eius, perspiciatis, cupiditate ipsa distinctio explicabo quisquam ut aliquid eligendi illum esse commodi, quos fuga reprehenderit. Perferendis, fugit distinctio commodi debitis illum accusamus cumque. Neque, eveniet ducimus!
+                Tempore nulla magnam voluptas, tenetur error laboriosam pariatur, atque suscipit voluptate, enim optio necessitatibus! Facilis distinctio illum, cupiditate dolorum numquam eligendi mollitia quam enim, illo, nesciunt molestias quaerat nulla fuga.
+                Cum laboriosam autem nam tenetur, sunt veritatis commodi ullam dolor. Odit illum ex officia doloremque praesentium, veniam magnam minus vel quidem. Blanditiis dignissimos vel explicabo ut, omnis maxime facere? Ipsam?
+                Unde sunt, libero aut harum voluptatibus illum, quis sit error itaque porro similique eaque velit explicabo eligendi impedit, voluptate fuga. Quo, minus rem. Temporibus voluptatum corporis, vero fugit labore soluta?
+                Rerum, recusandae sapiente modi reiciendis aperiam voluptatem quis? Quod velit quo culpa consequuntur repellat, exercitationem fuga maxime eligendi, qui numquam, aperiam totam excepturi? Animi fuga voluptatum provident minus eum illo!
+                Accusantium officia maxime non ex accusamus inventore incidunt earum in consequatur ea voluptatum dolores aliquam, necessitatibus magnam recusandae alias. Consectetur hic nostrum culpa iusto voluptatum animi adipisci nobis ducimus alias?
+                Illo magni qui totam dolore laudantium impedit officia, praesentium deserunt ipsa quis quidem cumque laborum commodi atque, voluptatem assumenda sapiente earum vel? Rerum ex animi culpa quos dignissimos consequuntur magni.
+                Eligendi iste accusantium dolore earum nihil est iusto itaque deserunt odio, non quas iure doloremque praesentium vel. Quia, dolor ullam sapiente enim neque deserunt minus voluptate maiores adipisci officiis odit.
+                Distinctio, explicabo hic praesentium quis rerum incidunt tenetur quam non asperiores. Quaerat quia, maxime qui, natus voluptates nemo est saepe necessitatibus, repellat aliquam sapiente id. Corporis tempora itaque sint enim.
+                Commodi sapiente repudiandae eum iste nemo. Enim inventore fugit consequatur totam a ipsam laudantium, amet quo corrupti consectetur optio praesentium dolor omnis veritatis, cum quod pariatur vero eligendi quae nisi!
+                Hic quisquam et molestias enim ipsum quibusdam nostrum assumenda quasi dolorem suscipit magni inventore quos aliquid adipisci provident, quidem ex repellendus! Voluptates, cum laudantium! Dolorum excepturi animi porro iusto impedit?
+                Repellat vitae eius aspernatur. Quidem laudantium accusamus modi cupiditate autem, dolorum alias fuga consequuntur officia, quia accusantium! Unde cumque voluptatem quia ut? Iusto saepe, blanditiis repudiandae alias culpa fuga voluptatum.
+                Id, inventore ipsa pariatur veniam dicta molestiae nostrum soluta quas rem temporibus nam unde aliquid quaerat incidunt modi voluptatibus maxime repellendus illo harum culpa itaque ab? Rerum eaque sint velit.
+                Laudantium maiores unde harum eius recusandae dolor eaque magni delectus asperiores cumque molestiae totam id autem blanditiis, vero fuga veniam doloribus odit illum natus. Pariatur repudiandae maxime distinctio exercitationem est.
+                Illo facere consequatur neque molestiae doloremque. Non at eaque in itaque repellendus nemo placeat numquam velit architecto est laborum, distinctio perspiciatis aut odio, rerum eum aspernatur iusto blanditiis eligendi a!</h1>
+            </div>
+        )
+    }
+}
+
+export default Latihan
